4a7d97cbd289a4e9593915bd672fe087544d31a3
yunb12345 <agusyoon11@hotmail.com>
2025-05-07 12:28:22 -0300

    cambio en acerca de m

--- src/components/Acerca.jsx
+++ src/components/Acerca.jsx
@@ -15,9 +15,9 @@ const Acerca = () => {
             className="flex flex-wrap">
                 <div className="w-full lg:p-8">
                     <div className="flex items-center justify-center">
-                        <p className="my-2 maxw-xl py-6 px-10">Soy un estudiante de 22 años que esta aprendiendo a programar en una tecnicatura de desarrollo en software. En mis primeros años de cursada aprendi lo basico de HTML, CSS y PHP para armar mi primera pagina eCommerce ademas de ver algoritmos basicos utilizando Python. 
-                            Actualmente estoy en la mitad de mi 2do año aprendiendo algoritmos avanzados con Java y creando proyectos por mi cuenta. Gracias a esto, estoy utilizando github en mi pasatiempo
-                            y espero en el futuro seguir creando mas proyectos o ser parte de un equipo en el que pueda afrontar nuevas tematicas.</p>
+                        <p className="my-2 maxw-xl py-6 px-10">Actualmente cursando una Tecnicatura en Desarrollo de Software, con experiencia en HTML, CSS, PHP y Python. 
+                            Desarrollé mi primer eCommerce y, actualmente, estoy enfocado en algoritmos avanzados con Java. 
+                            Creo proyectos personales y gestiono versiones en GitHub, buscando seguir aprendiendo y contribuir en equipos de desarrollo.</p>
                     </div>
                 </div>
             </motion.div>
